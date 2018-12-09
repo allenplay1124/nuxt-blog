9efb4dc55e5f2d07e6d219a8422839d41acee643
Allenplay <jailin1124@gmail.com>
2018-12-09 21:57:10 +0800

    sidebar and header

---
+++ components/sidebar.vue
@@ -0,0 +1,46 @@
+<template lang="html">
+<aside>
+  <mu-paper :z-depth="1" class="demo-list-wrap">
+    <div>
+      <mu-appbar color="teal">
+        最新文章
+      </mu-appbar>
+      <mu-list>
+        <mu-list-item href v-for="item in lastPosts">
+          <mu-list-item-title>
+              <a :href="item.link">{{item.title.rendered}}</a>
+          </mu-list-item-title>
+        </mu-list-item>
+      </mu-list>
+    </div>
+  </mu-paper>
+</aside>
+</template>
+
+<script>
+import axios from '~/plugins/axios'
+export default {
+  name: 'sidebar',
+  props: {
+
+  },
+  data() {
+    return {
+      lastPosts: []
+    }
+  },
+  created() {
+    this.getLastPosts()
+  },
+  methods: {
+    async getLastPosts() {
+      const res = await axios.get('wp/v2/posts')
+      this.lastPosts = res.data
+      console.log(res.data)
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ components/site-header.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+<header>
+<mu-appbar style="width: 100%;" color="primary">
+  <mu-button icon slot="left">
+    <mu-icon value="menu"></mu-icon>
+  </mu-button>
+  {{siteName}}-
+  <small>{{siteDescription}}</small>
+</mu-appbar>
+
+</header>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+export default {
+  props: {
+    siteName: String,
+    siteDescription: String
+  }
+}
+</script>
+
+<style lang="css">
+</style>
