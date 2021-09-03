0ea9f01634fb002a08015372e3fb16d27a91dc6b
jalin <jailin1124@gmail.com>
2021-09-03 22:19:56 +0800

    加入 topbar，設定佈景樣式

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <topBar></topBar>
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+import topBar from '~/components/home/topbar'
+export default {
+    components: {
+        topBar
+    }
+}
+</script>
