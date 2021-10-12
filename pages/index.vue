ef50fb0fb31731d2dddfc7044c7bf307e560b106
jalin <jailin1124@gmail.com>
2021-10-12 21:10:07 +0800

    載入 slider 資料

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <carousel></carousel>
+        <carousel :data="carouselData"></carousel>
         <featured></featured>
         <new-post></new-post>
         <footbar></footbar>
@@ -13,11 +13,26 @@ import featured from '~/components/home/featured'
 import newPost from '~/components/home/newPost'
 import footbar from '~/components/home/footbar'
 export default {
+    async asyncData({ $content }) {
+        const carouselData = await $content('articles')
+            .only(['title', 'summary', 'image'])
+            .where({
+                isSlider: true,
+                isOnline: true,
+            })
+            .sortBy("createdAt", "desc")
+            .fetch();
+        console.log(carouselData);
+        return {
+            carouselData
+        }
+    },
     components: {
         carousel,
         featured,
         newPost,
         footbar
     },
+
 }
 </script>
