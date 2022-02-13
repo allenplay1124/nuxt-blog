aa89daca2c075138d1c1f6fc8bb667eacc2d5d7c
Allenplay <jailin1124@gmail.com>
2022-02-13 12:44:58 +0800

    更新到 tailwindCSS3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,8 @@
 module.exports = {
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-        extend: {},
-        screens: {
-            //小手機
-            'xs': '576px',
-            //大螢幕手機
-            'sm': '640px',
-            //平板
-            'md': '768px',
-            //一般螢幕
-            'lg': '992px',
-            //大螢幕
-            'xl': '1200px',
-            //高解析度螢幕
-            '2xl': '1400px',
-        },
-        theme: {
-            backgroundSize: {
-                auto: 'auto',
-                cover: 'cover',
-                contain: 'contain',
-            },
+    tailwindcss: {
+        config: {
+            /* Extend the Tailwind config here */
+            content: ['content/**/**.md'],
         },
     },
-    variants: {
-        extend: {
-            divideColor: ['group-hover', 'hover'],
-            backgroundColor: ['group-hover', 'hover'],
-            boxShadow: ['group-hover', 'hover'],
-            fontSize: ['group-hover', 'hover'],
-            backgroundSize: ['group-hover', 'hover'],
-            scale: ['group-hover', 'hover'],
-            backgroundPosition: ['group-hover', 'hover'],
-            transition: ['group-hover', 'hover'],
-            translate: ['group-hover', 'hover'],
-        },
-    },
-    plugins: [],
-    purge: {
-        content: [
-            'content/**/**.md'
-        ]
-    }
 }
