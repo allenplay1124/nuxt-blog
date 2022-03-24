e6178c856865fe4e4b36814fa0041003d6b4ce75
allenplay <jailin1124@gmail.com>
2022-03-24 23:46:29 +0800

    加入 @tailwindcss/typography 插件，優化 markdown 樣式

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 module.exports = {
-    darkMode: 'class',
-    tailwindcss: {
-        config: {
-            /* Extend the Tailwind config here */
-            content: ['content/**/**.md'],
-        },
+  darkMode: 'class',
+  tailwindcss: {
+    config: {
+      /* Extend the Tailwind config here */
+      content: ['content/**/**.md'],
     },
-    content: [
-        `components/**/*.{vue,js}`,
-        `layouts/**/*.vue`,
-        `pages/**/*.vue`,
-        `plugins/**/*.{js,ts}`,
-        `nuxt.config.{js,ts}`,
-    ],
+  },
+  content: [
+    `components/**/*.{vue,js}`,
+    `layouts/**/*.vue`,
+    `pages/**/*.vue`,
+    `plugins/**/*.{js,ts}`,
+    `nuxt.config.{js,ts}`,
+  ],
+  plugins: [
+    require('@tailwindcss/typography'),
+    // ...
+  ],
 }
