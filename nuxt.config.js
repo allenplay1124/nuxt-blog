25616cc9392b8d58de930cec44e383451851389b
Allenplay <jailin1124@gmail.com>
2022-04-05 12:49:14 +0800

    移除發布設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,92 +1,92 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+    // Target: https://go.nuxtjs.dev/config-target
+    target: 'static',
 
-  loading: {
-    color: '#FF00CC',
-    height: '5px',
-  },
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: '艾玩不累格',
-    htmlAttrs: {
-      lang: 'zh-Hant-TW',
+    loading: {
+        color: '#FF00CC',
+        height: '5px',
     },
-    meta: [
-      { charset: 'utf-8' },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: '艾玩不累格',
+        htmlAttrs: {
+            lang: 'zh-Hant-TW',
+        },
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+            },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' },
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+
+    env: {
+        title: '艾玩不累格',
+    },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
+        { src: '~/assets/scss/index.scss', lang: 'scss' },
+        { src: '~/assets/scss/nuxt-content.scss', lang: 'scss' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
 
-  env: {
-    title: '艾玩不累格'
-  },
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+        // https://github.com/surmon-china/vue-awesome-swiper
+        // https://github.com/surmon-china/surmon-china.github.io/tree/source/projects/vue-awesome-swiper/nuxt
+        { src: '~/plugins/carousel', ssr: false },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    { src: '~/assets/scss/index.scss', lang: 'scss' },
-    { src: '~/assets/scss/nuxt-content.scss', lang: 'scss' },
-  ],
+        { src: '~/plugins/disqus', ssr: true },
+    ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    // https://github.com/surmon-china/vue-awesome-swiper
-    // https://github.com/surmon-china/surmon-china.github.io/tree/source/projects/vue-awesome-swiper/nuxt
-    { src: '~/plugins/carousel', ssr: false },
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
 
-    { src: '~/plugins/disqus', ssr: true },
-  ],
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://github.com/nuxt-community/color-mode-module
+        '@nuxtjs/color-mode',
+        // https://go.nuxtjs.dev/tailwindcss
+        '@nuxtjs/tailwindcss',
+        // https://github.com/nuxt-community/fontawesome-module#readme
+        '@nuxtjs/fontawesome',
+        //https://github.com/ivodolenc/nuxt-gsap-module
+        'nuxt-gsap-module',
+    ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: ['@nuxt/content'],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://github.com/nuxt-community/color-mode-module
-    '@nuxtjs/color-mode',
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-    // https://github.com/nuxt-community/fontawesome-module#readme
-    '@nuxtjs/fontawesome',
-    //https://github.com/ivodolenc/nuxt-gsap-module
-    'nuxt-gsap-module',
-  ],
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {},
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ['@nuxt/content'],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
-
-  tailwindcss: {
-    cssPath: '~/assets/css/tailwind.css',
-    jit: true,
-    // add '~tailwind.config` alias
-    exposeConfig: true,
-    configPath: 'tailwind.config.js',
-  },
-
-  colorMode: {
-    classSuffix: '',
-  },
-
-  fontawesome: {
-    component: 'fa',
-    icons: {
-      solid: true,
-      brands: true,
-      regular: true,
+    tailwindcss: {
+        cssPath: '~/assets/css/tailwind.css',
+        jit: true,
+        // add '~tailwind.config` alias
+        exposeConfig: true,
+        configPath: 'tailwind.config.js',
     },
-  },
 
-  gsap: {
-    /* Module Options */
-  },
+    colorMode: {
+        classSuffix: '',
+    },
+
+    fontawesome: {
+        component: 'fa',
+        icons: {
+            solid: true,
+            brands: true,
+            regular: true,
+        },
+    },
+
+    gsap: {
+        /* Module Options */
+    },
 }
