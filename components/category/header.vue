35a7601e0ba2bae2b6d54948ca1143f8bbe2aa6e
Allenplay <jailin1124@gmail.com>
2022-06-11 21:50:11 +0800

    微調頁首

--- components/category/header.vue
+++ components/category/header.vue
@@ -1,9 +1,23 @@
 <template>
     <div class="relative">
         <img :src="category.src" class="object-cover" :alt="category.title" />
-        <div class="absolute top-4 left-4 bg-gray-100 p-2 rounded shadow">
+        <div
+            class="
+                absolute
+                top-2
+                left-6
+                md:top-4 md:left-4
+                lg:top-6 lg:left-6
+                bg-[rgba(255,255,255,0.3)]
+                backdrop-blur-sm
+                p-1
+                rounded
+                text-gray-900
+                shadow
+            "
+        >
             <h1 class="text-2xl">{{ category.title }}</h1>
-            <div class="text-gray-500 py-2">
+            <div class="py-1">
                 <nuxt-content :document="category" />
             </div>
         </div>
--- components/home/carousel.vue
+++ components/home/carousel.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="carousel">
-        <swiper class="swiper" :option="swiperOption">
+    <div class="carousel relative">
+        <swiper class="swiper" :options="swiperOption">
             <swiper-slide>
                 <div class="relative w-full">
                     <img
@@ -17,11 +17,39 @@
                             text-center
                         "
                     >
-                        <div class="lg:container mx-auto pt-10">
-                            <h1 class="text-3xl">
+                        <div
+                            class="
+                                lg:container
+                                mx-auto
+                                pt-5
+                                md:pt-8
+                                lg:pt-20
+                                xl:pt-30
+                                2xl:pt-50
+                            "
+                        >
+                            <h1
+                                class="
+                                    site-title
+                                    text-5xl
+                                    md:text-5xl
+                                    lg:text-9xl
+                                    xl:text-9xl
+                                    2xl:text-9xl
+                                    font-black
+                                "
+                            >
                                 {{ this.$nuxt.$options.head.title }}
                             </h1>
-                            <div class="text-md">
+                            <div
+                                class="
+                                    site-description
+                                    text-md
+                                    mt-2
+                                    lg:mt-6
+                                    2xl:mt-8
+                                "
+                            >
                                 <span>軟體分享</span>
                                 <span>程式設計</span>
                                 <span>技術探討</span>
@@ -32,25 +60,43 @@
             </swiper-slide>
             <swiper-slide v-for="(item, index) in categories" :key="index">
                 <nuxt-link :to="item.path">
-                    <div>
+                    <div class="relative w-full">
                         <img
                             :src="item.src"
                             class="w-full object-center object-cover"
                         />
-                        <div>
-                            <div class="lg:container mx-auto pt-10">
+                        <div
+                            class="
+                                absolute
+                                w-full
+                                z-10
+                                top-0
+                                inset-y-20
+                                text-center
+                            "
+                        >
+                            <div
+                                class="
+                                    lg:container
+                                    mx-auto
+                                    pt-5
+                                    md:pt-10
+                                    lg:pt-40
+                                "
+                            >
                                 <div
                                     class="
                                         bg-[rgba(255,255,255,0.3)]
-                                        p-5
+                                        p-2
                                         text-gray-900
                                         backdrop-blur-sm
+                                        text-center
                                     "
                                 >
-                                    <div class="text-2xl">
+                                    <div class="text-2xl text-center">
                                         {{ item.title }}
                                     </div>
-                                    <div class="text-md">
+                                    <div class="text-md text-center">
                                         <nuxt-content :document="item" />
                                     </div>
                                 </div>
@@ -71,6 +117,7 @@
 <script>
 import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
 import 'swiper/css/swiper.css'
+
 export default {
     components: {
         Swiper,
@@ -104,6 +151,28 @@ export default {
 </script>
 
 <style scoped>
-.swiper-title {
+.site-title {
+    background: linear-gradient(180deg, rgb(0, 123, 255), rgb(0, 229, 255));
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+}
+.dark .site-title {
+    background: linear-gradient(180deg, red, yellow);
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+}
+
+.site-description {
+    font-size: 1.2rem;
+    font-weight: lighter;
+    color: rgb(0, 123, 255);
+}
+
+.dark .site-description {
+    font-size: 1.2rem;
+    font-weight: lighter;
+    color: red;
 }
 </style>
